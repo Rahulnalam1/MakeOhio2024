4e68bd1288fc31b2fc9ac663ce0647912ccc5ce6
Rahul Nalam <rahulnalam1@gmail.com>
2024-03-23 14:01:39 -0400

    pushed everything

--- autonomousdrone/src/App.js
+++ autonomousdrone/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MapComponent from './webGL'; // Ensure this path matches the location of your file
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Map App</h1>
       </header>
+      <MapComponent /> {/* This is where the MapComponent is rendered */}
     </div>
   );
 }
--- autonomousdrone/src/webGL.js
+++ autonomousdrone/src/webGL.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { AmbientLight, DirectionalLight, Matrix4, PerspectiveCamera, Scene, WebGLRenderer } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as THREE from 'three';
 
 const google = window.google;
 
